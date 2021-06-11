3973a0422189ca05498f53eca01bf5cb09b0024e
DESKTOP-CTVMTKB <firebase732@gmail.com>
2021-06-11 09:38:20 -0700

    add more activity

---
+++ app/src/main/java/com/example/azan/wudu.java
@@ -0,0 +1,14 @@
+package com.example.azan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wudu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wudu);
+    }
+}
